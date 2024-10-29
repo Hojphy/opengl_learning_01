d84cd4adb0a6133b62faa7ba6d3b12d7df554d96
robertkar <kareckirobert@gmail.com>
2024-10-29 23:26:56 +0100

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW\glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "chuj", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Okno sie zesralo\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "GLAD sie zesral\n";
+		glfwTerminate();
+		return -1;
+	}
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
